b8f96d87c33ce56fb94dd06ef96b0ad65c500373
GodT <Godtrb@Gmail.com>
2025-07-11 21:13:48 -0600

    AAOIJDOJASDOOJAIOHHOHO

--- Notas.py
+++ Notas.py
@@ -1,8 +1,27 @@
-object Alumno
+class Alumno:
+
+    def __init__(self, nombre, carne,Carrera,Nota_Final):
+        self.nombre = nombre
+        self.carne = carne
+        self.Carrera = Carrera
+        self.Nota_Final = Nota_Final
+
+def AlumnoReg(ListAlumn=[]):
+    name = input("Ingres el nombre del alumno:")
+    carne = int(input("Ingrese el carne del alumno"))
+    if carne>0:
+        Carrera=input("Ingrese la carrera del alumno:")
+        NotFin=int(input("Ingrese la nota final del alumno:"))
+        if (NotFin>=1|NotFin<=100):
+            newalumn=Alumno(name,carne,Carrera,NotFin)
+            ListAlumn.append(newalumn)
+        elif NotFin <0 |NotFin>100:
+            print("Nota invalida.")
+
 picker=0
 while picker !=4:
     print("-----Menu Veterinario-----")
-    print("1)Gestion clientes")
+    print("1)Registrar nuevo alumno")
     print("2)Gestion mascotas")
     print("3)Citas medicas")
     print("4)Salir")
