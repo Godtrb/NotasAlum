94923ec12d2f97c0ec562f17884535e392b4aa96
GodT <Godtrb@Gmail.com>
2025-07-11 21:06:03 -0600

    a

--- Notas.py
+++ Notas.py
@@ -1,1 +1,11 @@
-ayuda banda
+object Alumno
+picker=0
+while picker !=4:
+    print("-----Menu Veterinario-----")
+    print("1)Gestion clientes")
+    print("2)Gestion mascotas")
+    print("3)Citas medicas")
+    print("4)Salir")
+    picker= int(input("Seleccione una opcion:"))
+    match picker:
+        case 1:
